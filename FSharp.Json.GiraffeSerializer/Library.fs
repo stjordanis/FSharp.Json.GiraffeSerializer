1b52327d5b6453cd522c1555ce1c31997f7a49bf
Weslen <weslenng@outlook.com>
2021-05-15 14:23:42 -0300

    Initial commit

---
+++ FSharp.Json.GiraffeSerializer/Library.fs
@@ -0,0 +1,54 @@
+﻿namespace FSharp.Json.GiraffeSerializer
+
+open FSharp.Control.Tasks
+open FSharp.Json
+open Giraffe.Serialization.Json
+open System.IO
+open System.Text
+
+type FSharpJsonSerializer() =
+    interface IJsonSerializer with
+        member self.Deserialize<'T>(encoding: byte []) : 'T =
+            let serializer : IJsonSerializer = upcast self
+
+            Encoding.UTF8.GetString encoding
+            |> serializer.Deserialize<'T>
+
+        member _.Deserialize<'T>(payload: string) : 'T = Json.deserialize<'T> payload
+
+        member self.DeserializeAsync stream =
+            task {
+                use reader = new StreamReader(stream, Encoding.UTF8)
+
+                let serializer : IJsonSerializer = upcast self
+
+                return
+                    reader.ReadToEndAsync()
+                    |> Async.AwaitTask
+                    |> Async.RunSynchronously
+                    |> serializer.Deserialize<'T>
+            }
+
+        member self.SerializeToBytes serialize =
+            let serializer : IJsonSerializer = upcast self
+
+            serialize
+            |> serializer.SerializeToString
+            |> Encoding.UTF8.GetBytes
+
+        member self.SerializeToStreamAsync serialize stream =
+            let T =
+                task {
+                    use writer = new StreamWriter(stream, Encoding.UTF8)
+
+                    let serializer : IJsonSerializer = upcast self
+
+                    do!
+                        serialize
+                        |> serializer.SerializeToString
+                        |> writer.WriteAsync
+                }
+                
+            upcast T
+
+        member _.SerializeToString(serialize) = Json.serialize serialize
---
+++ README.md
@@ -0,0 +1,47 @@
+# FSharp.Json
+
+[Giraffe](https://github.com/giraffe-fsharp) `IJsonSerializer` implementation for [FSharp.Json](https://github.com/vsapronov/FSharp.Json)
+
+[![NuGet Info](https://buildstats.info/nuget/FSharp.Json.GiraffeSerializer?includePreReleases=true)](https://www.nuget.org/packages/FSharp.Json.GiraffeSerializer)
+
+## Usage
+```fsharp
+open FSharp.Json.GiraffeSerializer
+open Giraffe
+open Giraffe.Serialization.Json
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+type U = Killua of string
+
+type T = { Killua: U option }
+
+let router =
+    choose [ route "/"
+             >=> (fun next context -> json { Killua = Some <| U.Killua "Zoldyck" } next context) ]
+
+type Startup() =
+    member __.ConfigureServices(services: IServiceCollection) =
+        services.AddSingleton<IJsonSerializer>(FSharpJsonSerializer())
+        |> ignore
+
+        services.AddGiraffe() |> ignore
+
+    member __.Configure(builder: IApplicationBuilder) = builder.UseGiraffe router
+
+[<EntryPoint>]
+let main args =
+    Host
+        .CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(fun builder -> builder.UseStartup<Startup>() |> ignore)
+        .Build()
+        .Run()
+
+    0
+```
+
+## License
+
+This project is distributed under the [Apache License 2.0](LICENSE)
